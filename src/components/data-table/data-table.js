8debae9ceb65bb607baa0c688a11fa14eefe56d5
Kacper Nowosielski <kacper.nowosielski@gmail.com>
2019-05-25 10:38:00 +0300

    construct data output now

--- src/components/data-table/data-table.js
+++ src/components/data-table/data-table.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import '.././../App.css';
 import Data from './../../data/spells.json';
+import ClassSelector from './../selectors/classSelector'
+import LevelSelector from './../selectors/levelSelector'
+import SchoolSelector from './../selectors/schoolSelector'
 
 class DataTable extends Component {
   constructor(props) {
@@ -10,27 +13,37 @@ class DataTable extends Component {
 
   render() {
     return (
-      <div className={"dndapp-data"}>
-      {Data.map((spell, i) => {
-        return (
-          <div className={"spell-info"} key={i}>
-            <div className={"spell-dropdown"}>
-              <div className={"spell-name"}>
-                {spell.s_name}
-              </div>
-              <div className={"spell-definitions hidden"}>
-                <div className={"spell-top-level"}>Level {spell.s_lvl} {spell.s_school} spell</div>
-                <div className={"spell-casting-time"}>Casting Time: {spell.s_cast_time}</div>
-                <div className={"spell-range"}>Range: {spell.s_range} feet</div>
-                <div className={"spell-components"}>Components: {spell.s_components}</div>
-                <div className={"spell-duration"}>Duration {spell.s_duration}</div>
-                <div className={"spell-description"}>{spell.s_description}</div>
+      <div className={"dndapp-table"}>
+
+        <div className={"dndapp-selectors"}>
+          <ClassSelector onClick={() => Data.classFilter('c_class')}/>
+          <LevelSelector/>
+          <SchoolSelector/>
+        </div>
+
+        <div className={"dndapp-data"}>
+        {Data.map((spell, i) => {
+          return (
+            <div className={"spell-info"} key={i}>
+              <div className={"spell-dropdown"}>
+                <div className={"spell-name"}>
+                  {spell.s_name}
+                </div>
+                <div className={"spell-definitions hidden"}>
+                  <div className={"spell-top-level"}>Level {spell.s_lvl} {spell.s_school} spell</div>
+                  <div className={"spell-casting-time"}>Casting Time: {spell.s_cast_time}</div>
+                  <div className={"spell-range"}>Range: {spell.s_range} feet</div>
+                  <div className={"spell-components"}>Components: {spell.s_components}</div>
+                  <div className={"spell-duration"}>Duration {spell.s_duration}</div>
+                  <div className={"spell-description"}>{spell.s_description}</div>
+                </div>
               </div>
             </div>
-          </div>
-        );
-      })}
-    </div>
+          );
+        })}
+        </div>
+
+      </div>
     )
   }
 
--- src/components/output/output.js
+++ src/components/output/output.js
@@ -3,32 +3,37 @@ import '.././../App.css';
 import Data from './../../data/spells.json';
 
 // Load selector somponents.
-import ClassSelector from './../selectors/classSelector'
-import LevelSelector from './../selectors/levelSelector'
-import SchoolSelector from './../selectors/schoolSelector'
 import DataTable from './../data-table/data-table'
 
 // Output Component
 class Output extends Component {
   constructor(props) {
     super(props);
-    this.state = {Data};
-  }
+    this.state = {
+      data: Data
+    };
+
+    this.classFilter = this.classFilter.bind(this);
+  };
+
+  classFilter(c_class) {
+    this.setState({
+      data: this.data
+    });
+  };
 
   render() {
     return (
       <div className={"dndapp-wrapper"}>
-        <div className={"dndapp-selectors"}>
-          <ClassSelector/>
-          <LevelSelector/>
-          <SchoolSelector/>
-        </div>
-        <DataTable/>
+        <DataTable
+          data={this.state.data}
+          classFilter={this.classFilter}
+        />
       </div>
     );
 
-  }
+  };
 
-}
+};
 
 export default Output;

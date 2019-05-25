2ee686b739fcc9f44208c6bb4290a01870533c15
Kacper Nowosielski <kacper.nowosielski@gmail.com>
2019-05-25 22:42:17 +0300

    getting shite to work finally

--- src/components/data-table/data-table.js
+++ src/components/data-table/data-table.js
@@ -69,23 +69,29 @@ class DataTable extends Component {
     return (
       <div className={"spell-wrap"}>
         {this.state.data.map((spell, i) => {
-          return (
-            <div className={"spell-info"} key={i}>
-              <div className={"spell-dropdown"}>
-                <div className={"spell-name"}>
-                  {spell.s_name}
-                </div>
-                <div className={"spell-definitions hidden"}>
-                  <div className={"spell-top-level"}>Level {spell.s_lvl} {spell.s_school} spell</div>
-                  <div className={"spell-casting-time"}>Casting Time: {spell.s_cast_time}</div>
-                  <div className={"spell-range"}>Range: {spell.s_range} feet</div>
-                  <div className={"spell-components"}>Components: {spell.s_components}</div>
-                  <div className={"spell-duration"}>Duration {spell.s_duration}</div>
-                  <div className={"spell-description"}>{spell.s_description}</div>
+          if (this.state.data.map(s => spell.s_school).indexOf(this.state.filterSchool)) {
+            return (
+              <div className={"spell-info"} key={i}>
+                <div className={"spell-dropdown"}>
+                  <div className={"spell-name"}>
+                    {spell.s_name}
+                  </div>
+                  <div className={"spell-definitions hidden"}>
+                    <div className={"spell-top-level"}>Level {spell.s_lvl} {spell.s_school} spell</div>
+                    <div className={"spell-casting-time"}>Casting Time: {spell.s_cast_time}</div>
+                    <div className={"spell-range"}>Range: {spell.s_range} feet</div>
+                    <div className={"spell-components"}>Components: {spell.s_components}</div>
+                    <div className={"spell-duration"}>Duration {spell.s_duration}</div>
+                    <div className={"spell-description"}>{spell.s_description}</div>
+                  </div>
                 </div>
               </div>
-            </div>
-          )
+            )
+          }
+          else {
+            return ''
+          }
+
         })}
       </div>
     )

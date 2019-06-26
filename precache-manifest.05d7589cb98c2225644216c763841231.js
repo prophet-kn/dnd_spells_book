b6e96cecd2a91ca7f6f48145c05ecd2ea4166cf0
Kacper Nowosielski <kacper.nowosielski@gmail.com>
2019-06-26 17:19:36 +0300

    Updates

--- precache-manifest.05d7589cb98c2225644216c763841231.js
+++ precache-manifest.05d7589cb98c2225644216c763841231.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "dfe904e238d689fa6bc3c892b28949d1",
+    "revision": "85972dac0ec54c64f4aec796c759ccfd",
     "url": "/dnd_spells_book/index.html"
   },
   {
-    "revision": "4b61b6a207d9d9fc9b94",
-    "url": "/dnd_spells_book/static/css/main.c261af23.chunk.css"
+    "revision": "76cda329aad8c9cde2d2",
+    "url": "/dnd_spells_book/static/css/main.4febe2d6.chunk.css"
   },
   {
     "revision": "aaaaf76521a304288f05",
     "url": "/dnd_spells_book/static/js/2.d91511b4.chunk.js"
   },
   {
-    "revision": "4b61b6a207d9d9fc9b94",
-    "url": "/dnd_spells_book/static/js/main.8e831b86.chunk.js"
+    "revision": "76cda329aad8c9cde2d2",
+    "url": "/dnd_spells_book/static/js/main.559aae80.chunk.js"
   },
   {
     "revision": "132e73eca77a4f840f7e",

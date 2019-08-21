f11ad61ab745ab4460947d0deec007e8b6041812
Kacper Nowosielski <kacper.nowosielski@gmail.com>
2019-08-21 20:45:15 +0300

    gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp')
+var sass = require('gulp-sass')
+var bulkSass = require('gulp-sass-bulk-import')
+var sassLint = require('gulp-sass-lint')
+var cssDest = './src/'
+var scssSrc = './src/App.scss'
+var output = 'compressed'
+ 
+sass.compiler = require('node-sass')
+ 
+gulp.task('sass', function () {
+  return gulp.src(scssSrc)
+    .pipe(bulkSass())
+    .pipe(sassLint())
+    .pipe(sass({
+      outputStyle: output,
+      precision: 10,
+      includePaths: ['./src'],
+    }))
+    .on('error', sass.logError)
+    .pipe(gulp.dest(cssDest))
+})
+ 
+gulp.task('sass:watch', gulp.series(['sass'], function() {
+  gulp.watch(scssSrc, gulp.series(['sass']))
+}))
+
+gulp.task('default', gulp.parallel(['sass:watch']))

6336f126d96d215be08d238d1eddf623ed30665e
prophet-kn <kacper.nowosielski@gmail.com>
2019-08-30 21:52:28 +0300

    let's-a-see

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className={"dndapp dark-theme"}>
       <header className={"dndapp-header"}>
         Prophet&#39;s Companion<br></br>
-        v0.44:41ph4
+        v0.45:41ph4
       </header>
       <div className={"dndapp-body"}>
         <Output />

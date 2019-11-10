95fe2160dd365492ec2c67ac172bc0cddfd9b01e
prophet-kn <kacper.nowosielski@gmail.com>
2019-11-10 20:34:06 +0200

    moar

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <div className={"dndapp"}>
         <header className={"dndapp-header"}>
           Prophet&#39;s Companion<br></br>
-          v0.45:41ph4
+          v0.46:41ph4
         </header>
         <div className={"dndapp-body"}>
           <Output />
--- src/components/output/output.js
+++ src/components/output/output.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import DataTable from './../data-table/data-table'
+import DiceRolls from './../dice-roll/dice-roll'
 
 class Output extends Component {
   constructor(props) {
@@ -9,8 +10,8 @@ class Output extends Component {
     }
   }
 
-  pickView(e) {
-    const thisView = e.target.getAttribute('view')
+  pickView = (e) => {
+    let thisView = e.target.getAttribute('view')
 
     this.setState({
       currentView: thisView
@@ -18,7 +19,7 @@ class Output extends Component {
   }
 
   goBack = () => {
-    if (this.state.currentView !== 'default') {
+  if (this.state.currentView !== 'default') {
       return (
         <svg onClick={(e) => this.setState({currentView: 'default'})} width="30" height="30" className={"go-back"} viewBox="0 0 14 16" version="1.1" aria-hidden="true"><path fillRule="evenodd" d="M6 3.5c3.92.44 8 3.125 8 10-2.312-5.062-4.75-6-8-6V11L.5 5.5 6 0v3.5z"></path></svg>
       )
@@ -30,22 +31,28 @@ class Output extends Component {
       return (
         <div className={'dndapp-lander'}>
           <div className={'lander-button'} view={'dataTable'} onClick={(e) => {this.pickView(e)}}>
-            List of Spells
+            <span>List of Spells</span>
           </div>
           <div className={'lander-button'} view={'diceRoll'} onClick={(e) => {this.pickView(e)}}>
-            Dice Roll
+            <span>Dice Roll</span>
           </div>
-          <div className={'lander-button'} view={'soundEffects'} onClick={(e) => {this.pickView(e)}}>
-            Sound Effects
+          <div className={'lander-button disabled'} view={'characterSheet'}>
+            <span>Character Sheet</span>
           </div>
-          <div className={'lander-button'} view={'characterSheet'} onClick={(e) => {this.pickView(e)}}>
-            Character Sheet
+          <div className={'lander-button disabled'} view={'musicBoard'}>
+            <span>Music Board</span>
+          </div>
+          <div className={'lander-button disabled'} view={'NPCRandomizer'}>
+            <span>NPC Randomizer</span>
           </div>
         </div>
       )
     }
     else if (this.state.currentView === 'dataTable') {
-      return <DataTable/>
+      return <DataTable />
+    }
+    else if (this.state.currentView === 'diceRoll') {
+      return <DiceRolls />
     }
   }
 

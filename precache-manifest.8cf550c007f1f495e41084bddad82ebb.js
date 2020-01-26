040f2cebfd6807b703856a6df21f53753a5e862c
Kacper Nowosielski (prophet-kn) <kacper.nowosielski@gmail.com>
2020-01-26 22:26:28 +0200

    Updates

--- precache-manifest.8cf550c007f1f495e41084bddad82ebb.js
+++ precache-manifest.8cf550c007f1f495e41084bddad82ebb.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5ba5961b499fa5de679e241770cf7b81",
+    "revision": "9f9b42783a9e36e2a9db00b19c59806e",
     "url": "/dnd_spells_book/index.html"
   },
   {
-    "revision": "1d60e68d1e3fa823ec98",
-    "url": "/dnd_spells_book/static/css/main.4254f7e4.chunk.css"
+    "revision": "743299c38d7ede97a52d",
+    "url": "/dnd_spells_book/static/css/main.cd5ed2ed.chunk.css"
   },
   {
     "revision": "b9906dd5afaa2fb97dd2",
     "url": "/dnd_spells_book/static/js/2.9985252e.chunk.js"
   },
   {
-    "revision": "1d60e68d1e3fa823ec98",
-    "url": "/dnd_spells_book/static/js/main.c0cd848b.chunk.js"
+    "revision": "743299c38d7ede97a52d",
+    "url": "/dnd_spells_book/static/js/main.f4ae9920.chunk.js"
   },
   {
     "revision": "132e73eca77a4f840f7e",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dnd_spells_book/precache-manifest.55a07653a6d46e1f5a49d1e00e3ff651.js"
+  "/dnd_spells_book/precache-manifest.8cf550c007f1f495e41084bddad82ebb.js"
 );
 
 self.addEventListener('message', (event) => {

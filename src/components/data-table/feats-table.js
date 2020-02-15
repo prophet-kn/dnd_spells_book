07093bb478a09bd46492fbe39db8ba07b8227b75
Kacper Nowosielski (prophet-kn) <kacper.nowosielski@gmail.com>
2020-02-16 00:14:07 +0200

    PC-15: fix tiny thingie things.

--- src/components/data-table/feats-table.js
+++ src/components/data-table/feats-table.js
@@ -76,7 +76,7 @@ class FeatsTable extends Component {
   filterDropdowns() {
     return (
       <div className={this.state.filterButton === true ? "filter-dropdown active" : "filter-dropdown hidden"}>
-        <div className={"filter-close"} onClick={this.onClickFilter.bind(this)}></div>
+        <svg width="30" height="30" viewBox="0 0 12 16" className={"filter-close"} onClick={this.onClickFilter.bind(this)}><path fillRule="evenodd" d="M7.48 8l3.75 3.75-1.48 1.48L6 9.48l-3.75 3.75-1.48-1.48L4.52 8 .77 4.25l1.48-1.48L6 6.52l3.75-3.75 1.48 1.48L7.48 8z"></path></svg>
         <FilterDataButtons title={'Race prerequisite'} values={uniqueRacePrerequisite} setFilter={this.setFilter} />
         <FilterDataButtons title={'Skill prerequisite'} values={uniqueSkillPrerequisite} setFilter={this.setFilter} />
       </div>

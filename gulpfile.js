40941cf5526351615eddc795bced0d00a0775982
Lois Overvoorde <loisovervoorde@gmail.com>
2020-05-18 18:34:06 +0300

    Initial working commit.

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,39 @@
 var gulp = require('gulp')
 var sass = require('gulp-sass')
-var bulkSass = require('gulp-sass-bulk-import')
+var autoprefixer = require("gulp-autoprefixer");
 var concat = require('gulp-concat')
-var cssDest = './src/compiled/'
+var clean = require('gulp-clean-css')
+
+var cssDest = 'src/compiled'
 var output = 'compressed'
-sass.compiler = require('node-sass')
+//sass.compiler = require('node-sass')
 
 // Watch all scss changes.
 var source = './src/styles'
 
+var sassOptions = {
+  errLogToConsole: true,
+  outputStyle: "nested"
+};
+
+var autoprefixerOptions = {
+  cascade: false,
+  grid: true,
+  remove: false
+};
+
 gulp.task('sass:all', function () {
   return gulp
-    .src('./src/styles/compile/_compile.scss')
-    .pipe(bulkSass())
-    .pipe(concat('theme.css'))
-    .pipe(sass({
-      outputStyle: output,
-      precision: 10,
-      includePaths: ['./src/styles/'],
-    }))
+    .src('./src/styles/**/*.scss')
+    .pipe(sass())
     .on('error', sass.logError)
+    .pipe(autoprefixer())
+    .pipe(clean())
     .pipe(gulp.dest(cssDest))
 })
 
 gulp.task('sass', gulp.series(['sass:all']))
- 
+
 gulp.task('sass:watch', gulp.series(['sass'], function() {
   gulp.watch(source, gulp.series(['sass']))
 }))

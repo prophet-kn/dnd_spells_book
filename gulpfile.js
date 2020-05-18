7f896674da2940c62872cdd980952566205a2688
Lois Overvoorde <loisovervoorde@gmail.com>
2020-05-18 20:05:22 +0300

    After some more tidying.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp')
 var sass = require('gulp-sass')
-var bulkSass = require('gulp-sass-bulk-import');
-var autoprefixer = require('gulp-autoprefixer');
+var bulkSass = require('gulp-sass-bulk-import')
+var autoprefixer = require('gulp-autoprefixer')
 var clean = require('gulp-clean-css')
 
 var input = 'src/styles'

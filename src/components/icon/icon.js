b8950b63269452f8e9dbb546b10fd225fdbeeff3
Lois Overvoorde <loisovervoorde@gmail.com>
2020-07-17 15:41:20 +0300

    Fix fire icon, update icon component.

--- src/components/icon/icon.js
+++ src/components/icon/icon.js
@@ -6,7 +6,6 @@ class Icon extends React.Component {
     return (
       <ReactSVG
         src = {"svgs/" + this.props.title + ".svg"}
-        key = {this.props.key}
         className = "item-icon"
         role = "img"
         aria-label = {this.props.title}
@@ -16,14 +15,9 @@ class Icon extends React.Component {
             const title = document.createElement('title')
             title.innerHTML = this.props.title
             svg.prepend(title)
+            path.appendChild(title)
 
-            const titleq = document.createElement('title')
-            titleq.innerHTML = this.props.title
-            path.appendChild(titleq)
-
-            svg.setAttribute('height', '28px');
-            //svg.setAttribute('viewBox', '0 0 28 28');
-            //svg.setAttribute('transform', 'scale(0.1)');
+            svg.setAttribute('height', '28px')
           }
         }
       />

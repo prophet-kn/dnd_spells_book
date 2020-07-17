171e82fcdf890c59761ded13ca66ebc57c8b1ebc
Lois Overvoorde <loisovervoorde@gmail.com>
2020-07-17 22:30:58 +0300

    Set up eslint, also add sass-lint.

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ var sass = require('gulp-sass')
 var bulkSass = require('gulp-sass-bulk-import')
 var autoprefixer = require('gulp-autoprefixer')
 var clean = require('gulp-clean-css')
+var eslint = require('gulp-eslint');
+var sassLint = require('gulp-sass-lint')
 
 var input = 'src/styles'
 var output = 'src/compiled'
@@ -23,5 +25,19 @@ gulp.task('sass', gulp.series(['sass:all']))
 gulp.task('sass:watch', gulp.series(['sass'], function() {
   gulp.watch(input, gulp.series(['sass']))
 }))
+
+gulp.task('sass:lint', function () {
+  return gulp.src(['src/styles/**/*.scss'])
+    .pipe(sassLint())
+    .pipe(sassLint.format())
+    .pipe(sassLint.failOnError())
+});
+
+gulp.task('js:lint', function () {
+  return gulp.src(['src/components/**/*.js'])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+});
 
 gulp.task('default', gulp.parallel(['sass:watch']))

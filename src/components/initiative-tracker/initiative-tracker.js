f11fabd2b192b83ddfab8d44b9353c0ebc197cc4
Kacper Nowosielski <kacper.nowosielski@siili.com>
2020-07-27 12:48:20 +0300

    PC-19: init commits

---
+++ src/components/initiative-tracker/initiative-tracker.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class initiativeTracker extends Component {
+  constructor(props) {
+    super()
+    this.state = {
+      derp: ''
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        derpson
+      </div>
+    )
+  }
+}
+
+export default initiativeTracker

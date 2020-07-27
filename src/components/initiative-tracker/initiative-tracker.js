49335e3ce08ebc3ade6be8c0eed4d57a598fe1a5
Kacper Nowosielski <kacper.nowosielski@siili.com>
2020-07-27 13:11:17 +0300

    PC-19: tiny work

--- src/components/initiative-tracker/initiative-tracker.js
+++ src/components/initiative-tracker/initiative-tracker.js
@@ -4,14 +4,61 @@ class initiativeTracker extends Component {
   constructor(props) {
     super()
     this.state = {
-      derp: ''
+      items: 0
+    }
+  }
+
+  bars() {
+    const items = []
+
+    for (let i = 0; i < this.state.items; i += 1) {
+      items.push(
+        <div key={i}>
+          <input type={'number'}></input>
+          <input type={'text'}></input>
+          <input type={'number'}></input>
+          <input type={'number'}></input>
+        </div>
+      )
+    }
+
+    return (
+      <div>
+        init count, name, hp, add or remove
+        {items}
+      </div>
+    )
+  }
+
+  addItem() {
+    this.setState({
+      items: this.state.items + 1
+    })
+  }
+
+  removeItem() {
+    // Prevent going to negative values in count.
+    if (this.state.items !== 0) {
+      this.setState({
+        items: this.state.items - 1
+      })
+    } else {
+      this.setState({
+        items: 0
+      })
     }
   }
 
   render() {
     return (
       <div>
-        derpson
+        {this.bars()}
+        <div onClick={this.addItem.bind(this)}>
+          add
+        </div>
+        <div onClick={this.removeItem.bind(this)}>
+          remove
+        </div>
       </div>
     )
   }

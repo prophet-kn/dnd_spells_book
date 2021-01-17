91c2e3c0c0bf4ae75e828d5fa412dc55b3edda01
Lois Overvoorde <loisovervoorde@gmail.com>
2021-01-17 14:14:34 +0000

    Adding stat block to spell working.

---
+++ src/components/monster-item/monster-item.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import { MonsterDescription } from './monster-description'
+
+class MonsterItem extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      showList: false
+    }
+
+    this.addClassName = this.addClassName.bind(this)
+    this.getModifier = this.getModifier.bind(this)
+  }
+
+  addClassName(e, i) {
+    const monsterState = this.state
+    monsterState.showList = monsterState.showList === i ? false : i
+    this.setState(monsterState)
+  }
+
+  render() {
+    const monsterItem = this.props.monsterData
+    const i = monsterItem.name
+
+    return (
+      <div className={this.state.showList === i ? 'monster-dropdown' : 'monster-dropdown hide-child'}>
+        <div className={'monster-name'} onClick={ (e) => { this.addClassName(e, i) } }>
+          <span>{monsterItem.name}</span>
+          <div className={'monster-tooltip'}>CR: {monsterItem.Challenge}</div>
+          <svg className={this.state.showList === i ? 'chevron opened' : 'chevron'} width='30' height='30' viewBox='0 0 10 16'><path fillRule='evenodd' d='M5 11L0 6l1.5-1.5L5 8.25 8.5 4.5 10 6l-5 5z'></path></svg>
+        </div>
+        {this.state.showList === i ? <MonsterDescription monsterItem={monsterItem} i={i}/> : null}
+      </div>
+    )
+  }
+}
+
+export default MonsterItem
